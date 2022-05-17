2b025ccee92240fec063dd77f9e041919671f9a9
Shahim Essaid <shahim@essaid.com>
2022-05-17 11:07:36 -0700

    docker run on sample ig.

--- .tmp/jq.sh
+++ .tmp/jq.sh
@@ -1,15 +1,25 @@
 # jq examples
 
-LATEST="$(curl "https://api.github.com/repos/hl7/fhir-ig-publisher/releases/latest" | jq -c '.tag_name')"
-echo LATEST $LATEST
+# this is the current version selection code
 
-TO_BUILD="$(jq -c --argjson latest "$LATEST" '
-    [.build_versions
-    | if has($latest) then empty else $latest end,
-      (to_entries[] | select(.value.built != "yes" and .value.built != "failed").key)
-    ] | sort
-    ' ../builds.json)"
-echo TO BUILD: $TO_BUILD
+#LATEST="$(curl "https://api.github.com/repos/hl7/fhir-ig-publisher/releases/latest" | jq -c '.tag_name')"
+#echo LATEST $LATEST
+#
+#TO_BUILD="$(jq -c --argjson latest "$LATEST" '
+#    [.build_versions
+#    | if has($latest) then empty else $latest end,
+#      (to_entries[] | select(.value.built != "yes" and .value.built != "failed").key)
+#    ] | sort
+#    ' ../builds.json)"
+#echo TO BUILD: $TO_BUILD
+
+# this is the current builds.json update code with a hard coded variable
+
+jq --arg version "1.1.118"  '
+  . as $root |
+  .build_versions |
+  if has($version) then ( $root | .build_versions[$version]["built"] |= "yes" ) else ( $root | .build_versions[$version] |= {"built": "yes"}) end
+  '  ../builds.json
 
 #jq '.' ../config.json
 
